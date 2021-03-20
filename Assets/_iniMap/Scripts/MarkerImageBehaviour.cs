3825db4371ddd71ecb01227964025dbec2d56490
Hadid <blunty.hadid@gmail.com>
2021-03-20 17:50:40 +0500

    minimap commit

---
+++ Assets/_iniMap/Scripts/MarkerImageBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerImageBehaviour : MonoBehaviour
+{
+    public GameObject ParentObject;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        this.ParentObject.SetActive(false);
+    }
+}
